13b68240278775927aa063fcf8f743ee2d012f84
polyboly <barbarahpolette@gmail.com>
2026-05-09 12:13:13 -0500

    Update administrador.java
    new version

--- SistemaCitas/src/administrador.java
+++ SistemaCitas/src/administrador.java
@@ -1,12 +1,17 @@
-class Administrador extends Usuario {
+public class Administrador extends Usuario {
 
-    public Administrador(String username, String password) {
-        super(username, password);
+    public Administrador(String username, String correo, String password) {
+        super(username, correo, password);
     }
 
     @Override
     public void menu() {
-        System.out.println("1. Ver todas las citas");
-        System.out.println("2. Eliminar cita");
+        System.out.println("\n╔══════════════════════════════════╗");
+        System.out.println("║      MENU — ADMINISTRADOR        ║");
+        System.out.println("╠══════════════════════════════════╣");
+        System.out.println("║  1. Ver todas las citas          ║");
+        System.out.println("║  2. Eliminar una cita            ║");
+        System.out.println("║  3. Ver todos los historiales    ║");
+        System.out.println("╚══════════════════════════════════╝");
     }
 }
